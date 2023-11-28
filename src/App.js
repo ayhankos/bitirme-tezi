4ec28e9b0009e10a8e4cb5715a8261e6b5d87685
ayhankos <ayhanx926@gmail.com>
2023-11-28 03:25:19 +0300

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignInPage from "./components/SignIn";
+import SignUpPage from "./components/SignUp";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={SignUpPage} />
+        <Route path="/SignIn" component={SignInPage} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
