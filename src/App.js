dacdd4b4216b2512804c3b1827c4da2fbe0319b3
ayhankos <ayhanx926@gmail.com>
2023-11-29 03:24:36 +0300

    s

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import main from "./components/main";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={SignUp} />
         <Route path="/sign-in" component={SignIn} />
+        <Route path="/main" component={main} />
       </Switch>
     </Router>
   );
