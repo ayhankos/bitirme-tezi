6a430a2da0d6819b51c72bf785a66440255e1ac5
ayhankos <ayhanx926@gmail.com>
2023-11-29 04:10:29 +0300

    s

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -27,7 +27,7 @@ export const SignIn = () => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        username: data.get("email"),
+        email: data.get("email"),
         password: data.get("password"),
       }),
     })
