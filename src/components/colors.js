33d553f0cf06640781d946ac6dd172cae423efc5
ayhankos <ayhanx926@gmail.com>
2023-12-02 18:34:33 +0300

    s

---
+++ src/components/colors.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#eeeeee", // Örneğin, ana renk
+    },
+    secondary: {
+      main: "#e0e0e0", // Örneğin, ikincil renk
+    },
+    text: {
+      primary: "#424242", // Örneğin, metin rengi
+    },
+  },
+  // Diğer tema özellikleri buraya eklenebilir
+});
+
+export default theme;
