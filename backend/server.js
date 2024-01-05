66787e258b3d9012b97b9d31346929330dc34810
ayhankos <ayhanx926@gmail.com>
2024-01-05 23:18:18 +0300

    g

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5002;
 
 // Example: Adding a simple endpoint
 app.get("/", (req, res) => {
