dea3e45985ea5f6b25413e198fc04f790e85181c
ayhankos <ayhanx926@gmail.com>
2024-01-08 23:33:37 +0300

    Revert "hhh"
    This reverts commit d80267c32d365e1bc4ffd670429034e758ddf531.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-const bcrypt = require("bcrypt");
+const crypto = require("crypto");
 const cors = require("cors");
 
 const app = express();
@@ -24,7 +24,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "fenerbahce41",
-  database: "BitirmeTezi",
+  database: "BitirmeTezi", // Veritabanı adınızı buraya ekleyin
 });
 
 db.connect((err) => {
@@ -40,33 +40,27 @@ app.post("/register", (req, res) => {
     const { firstName, lastName, email, password } = req.body;
 
     // Şifreyi hashleme
-    bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
-      if (hashErr) {
-        console.error("Error hashing password:", hashErr);
-        return res.status(500).json({
-          success: false,
-          message: "Internal Server Error",
-          error: hashErr.message,
-        });
-      }
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
 
-      db.query(
-        "INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)",
-        [firstName, lastName, email, hashedPassword],
-        (err, result) => {
-          if (err) {
-            console.error("Error executing MySQL query:", err);
-            return res.status(500).json({
-              success: false,
-              message: "Internal Server Error",
-              error: err.message,
-            });
-          }
-
-          res.json({ success: true, message: "Registration successful" });
+    db.query(
+      "INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)",
+      [firstName, lastName, email, hashedPassword],
+      (err, result) => {
+        if (err) {
+          console.error("Error executing MySQL query:", err);
+          return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: err.message,
+          });
         }
-      );
-    });
+
+        res.json({ success: true, message: "Registration successful" });
+      }
+    );
   } catch (error) {
     console.error("Error in registration:", error);
     res.status(500).json({
@@ -81,53 +75,43 @@ app.post("/login", (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Şifreyi hashleme
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
     db.query(
-      "SELECT * FROM users WHERE email = ?",
-      [email],
-      (selectErr, results) => {
-        if (selectErr) {
-          console.error("Error executing MySQL query: " + selectErr.stack);
+      "SELECT * FROM users WHERE email = ? AND password = ?",
+      [email, hashedPassword],
+      (err, results) => {
+        if (err) {
+          console.error("Error executing MySQL query: " + err.stack);
           return res.status(500).json({
             success: false,
             message: "Internal Server Error",
-            error: selectErr.message,
+            error: err.message,
           });
         }
 
         if (results.length > 0) {
-          const user = results[0];
+          const user = results[0]; // İlk bulunan kullanıcıyı alıyoruz
 
-          // Parola karşılaştırması
-          bcrypt.compare(password, user.password, (compareErr, match) => {
-            if (compareErr) {
-              console.error("Error comparing passwords:", compareErr);
-              return res.status(500).json({
-                success: false,
-                message: "Internal Server Error",
-                error: compareErr.message,
-              });
-            }
-
-            if (match) {
-              // Kullanıcı bilgilerini geri döndür
-              res.json({
-                success: true,
-                message: "Login successful",
-                user: {
-                  id: user.id,
-                  firstName: user.firstName,
-                  lastName: user.lastName,
-                  email: user.email,
-                },
-              });
-            } else {
-              res
-                .status(401)
-                .json({ success: false, message: "Invalid credentials" });
-            }
+          // Kullanıcı bilgilerini geri döndür
+          res.json({
+            success: true,
+            message: "Login successful",
+            user: {
+              id: user.id,
+              firstName: user.firstName,
+              lastName: user.lastName,
+              email: user.email,
+            },
           });
         } else {
-          res.status(401).json({ success: false, message: "User not found" });
+          res
+            .status(401)
+            .json({ success: false, message: "Invalid credentials" });
         }
       }
     );
